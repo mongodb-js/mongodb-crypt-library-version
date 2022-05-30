afb481b3683b44410c663451d73d17f69a84e1b4
Anna Henningsen <anna@addaleax.net>
2022-05-30 16:39:48 +0200

    fixup: s/wget/curl -O/

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ FAILED=''
 
 if [ `node -p process.platform` = "linux" ]; then
   mkdir -p tmp && cd tmp && \
-    wget "https://mciuploads.s3.amazonaws.com/mongodb-mongo-master/mongo_csfle/enterprise-rhel-80-64-bit-dynamic-required/237b218974e4fc09104c81fe0bb1ba83688d8035/mongo_csfle_v1_dev-6.0.0-alpha-529-g237b218.tgz" && \
+    curl -O "https://mciuploads.s3.amazonaws.com/mongodb-mongo-master/mongo_csfle/enterprise-rhel-80-64-bit-dynamic-required/237b218974e4fc09104c81fe0bb1ba83688d8035/mongo_csfle_v1_dev-6.0.0-alpha-529-g237b218.tgz" && \
     tar xvzf mongo_csfle_v1_dev-6.0.0-alpha-529-g237b218.tgz && \
     ../bin/mongodb-crypt-library-version.js lib/mongo_csfle_v1.so > version && \
     cat version && \
@@ -15,7 +15,7 @@ if [ `node -p process.platform` = "linux" ]; then
   cd .. && rm -rf tmp || FAILED=1
 
   mkdir -p tmp && cd tmp && \
-    wget "https://downloads.mongodb.com/linux/mongo_csfle_v1-linux-x86_64-enterprise-ubuntu2004-5.3.0-rc3.tgz" && \
+    curl -O "https://downloads.mongodb.com/linux/mongo_csfle_v1-linux-x86_64-enterprise-ubuntu2004-5.3.0-rc3.tgz" && \
     tar xvzf mongo_csfle_v1-linux-x86_64-enterprise-ubuntu2004-5.3.0-rc3.tgz && \
     ../bin/mongodb-crypt-library-version.js lib/mongo_csfle_v1.so > version && \
     cat version && \
@@ -25,7 +25,7 @@ fi
 
 if [ `node -p process.platform` = "darwin" ]; then
   mkdir -p tmp && cd tmp && \
-    wget "https://mciuploads.s3.amazonaws.com/mongodb-mongo-master/mongo_csfle/enterprise-macos/237b218974e4fc09104c81fe0bb1ba83688d8035/mongo_csfle_v1-6.0.0-alpha-529-g237b218.tgz" && \
+    curl -O "https://mciuploads.s3.amazonaws.com/mongodb-mongo-master/mongo_csfle/enterprise-macos/237b218974e4fc09104c81fe0bb1ba83688d8035/mongo_csfle_v1-6.0.0-alpha-529-g237b218.tgz" && \
     tar xvzf mongo_csfle_v1-6.0.0-alpha-529-g237b218.tgz && \
     ../bin/mongodb-crypt-library-version.js lib/mongo_csfle_v1.dylib > version && \
     cat version && \
@@ -33,7 +33,7 @@ if [ `node -p process.platform` = "darwin" ]; then
   cd .. && rm -rf tmp || FAILED=1
 
   mkdir -p tmp && cd tmp && \
-    wget "https://downloads.mongodb.com/osx/mongo_csfle_v1-macos-x86_64-enterprise-5.3.0-rc3.tgz" && \
+    curl -O "https://downloads.mongodb.com/osx/mongo_csfle_v1-macos-x86_64-enterprise-5.3.0-rc3.tgz" && \
     tar xvzf mongo_csfle_v1-macos-x86_64-enterprise-5.3.0-rc3.tgz && \
     ../bin/mongodb-crypt-library-version.js lib/mongo_csfle_v1.dylib > version && \
     cat version && \
@@ -43,7 +43,7 @@ fi
 
 if [ `node -p process.platform` = "win32" ]; then
   mkdir -p tmp && cd tmp && \
-    wget "https://mciuploads.s3.amazonaws.com/mongodb-mongo-master/mongo_csfle/enterprise-windows-required/237b218974e4fc09104c81fe0bb1ba83688d8035/mongo_csfle_v1-6.0.0-alpha-529-g237b218.zip" && \
+    curl -O "https://mciuploads.s3.amazonaws.com/mongodb-mongo-master/mongo_csfle/enterprise-windows-required/237b218974e4fc09104c81fe0bb1ba83688d8035/mongo_csfle_v1-6.0.0-alpha-529-g237b218.zip" && \
     unzip mongo_csfle_v1-6.0.0-alpha-529-g237b218.zip && \
     ../bin/mongodb-crypt-library-version.js lib/mongo_csfle_v1.dll > version && \
     cat version && \
@@ -51,7 +51,7 @@ if [ `node -p process.platform` = "win32" ]; then
   cd .. && rm -rf tmp || FAILED=1
 
   mkdir -p tmp && cd tmp && \
-    wget "https://downloads.mongodb.com/windows/mongo_csfle_v1-windows-x86_64-enterprise-5.3.0-rc3.zip" && \
+    curl -O "https://downloads.mongodb.com/windows/mongo_csfle_v1-windows-x86_64-enterprise-5.3.0-rc3.zip" && \
     unzip mongo_csfle_v1-windows-x86_64-enterprise-5.3.0-rc3.zip && \
     ../bin/mongodb-crypt-library-version.js lib/mongo_csfle_v1.dll > version && \
     cat version && \
